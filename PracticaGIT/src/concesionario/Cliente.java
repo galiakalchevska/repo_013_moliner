ffdd8ba29907cf1b70ad9aaee1aa7201cb6626c4
kalchevska <galiakalchevska@gmail.com>
2015-05-01 10:01:51 +0200

    Cambios en Principal y Cliente

--- PracticaGIT/src/concesionario/Cliente.java
+++ PracticaGIT/src/concesionario/Cliente.java
@@ -1,5 +1,37 @@
 package concesionario;
 
+/**
+ * @author PUÑETEIRA
+ * @version 1.0
+ */
 public class Cliente {
 
+	private String nombre;
+	private String apellidos;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Cliente(String nombre, String apellidos) {
+		setNombre(nombre);
+		setApellidos(apellidos);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", apellidos=" + apellidos + "]";
+	}
 }
--- PracticaGIT/src/concesionario/Principal.java
+++ PracticaGIT/src/concesionario/Principal.java
@@ -1,5 +1,14 @@
 package concesionario;
 
+/**
+ * @author PUÑETEIRA
+ * @version 1.0
+ */
 public class Principal {
 
+	public static void main(String[] args) {
+
+		Cliente cliente = new Cliente("Javier", "García");
+		System.out.println(cliente);
+	}
 }
