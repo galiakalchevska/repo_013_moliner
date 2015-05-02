20dcc0769db844bc95258a1fa78ab38b72a94562
kalchevska <galiakalchevska@gmail.com>
2015-05-02 08:49:25 +0200

    Fichero ignorado

--- PracticaGIT/src/concesionario/Coche.java
+++ PracticaGIT/src/concesionario/Coche.java
@@ -2,4 +2,13 @@ package concesionario;
 
 public class Coche {
 
+	private String matricula;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 }
