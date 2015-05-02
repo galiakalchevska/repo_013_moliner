18856d7a113f0fa1c26a4c8cfb13d7dfae4fa2a5
kalchevska <galiakalchevska@gmail.com>
2015-05-02 08:45:48 +0200

    Segunda versón

--- PracticaGIT/src/concesionario/Concesionario.java
+++ PracticaGIT/src/concesionario/Concesionario.java
@@ -1,5 +1,18 @@
 package concesionario;
 
+import java.util.*;
+
 public class Concesionario {
 
+	private ArrayList<Coche> coches ;
+
+	public ArrayList<Coche> getCoches() {
+		return coches;
+	}
+
+	public void setCoches(ArrayList<Coche> coches) {
+		this.coches = coches;
+	}
+		
 }
+
